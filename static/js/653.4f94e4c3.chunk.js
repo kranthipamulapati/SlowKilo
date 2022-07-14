1e3300d55565268c5836956d4971f98f82ff53c2
Kranthi Pamulapati <pamulapati.kranthi@gmail.com>
2022-07-14 06:23:10 +0530

    Updates

---
+++ static/js/653.4f94e4c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkslowkilo=self.webpackChunkslowkilo||[]).push([[653],{653:function(e,n,t){t.r(n),t.d(n,{default:function(){return s}});var c=t(791),l=t(184);function s(){return(0,c.useEffect)((function(){document.title="Not Found - SlowKilo"})),(0,l.jsx)("div",{className:"bg-gray-background",children:(0,l.jsx)("div",{className:"mx-auth max-w-screen-lg",children:(0,l.jsx)("p",{className:"text-center text-2xl",children:"Not Found!"})})})}}}]);
+//# sourceMappingURL=653.4f94e4c3.chunk.js.map
